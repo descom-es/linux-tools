91f1949802e5fdd46d5442793fb4d3eac6ac00ff
César Garcia <cesar@descom.es>
2018-06-12 18:31:35 +0200

    bug --defaults-file

--- mysql/bin/backup.sh
+++ mysql/bin/backup.sh
@@ -24,7 +24,9 @@ if [ -z ${MYSQL_AUTH} ]; then
         MYSQL_PASS=`cat /etc/psa/.psa.shadow`
         MYSQL_AUTH=" -uadmin -p${MYSQL_PASS} "
     elif [ -f "/etc/mysql/debian.cnf" ];then
-        MYSQL_AUTH=" --defaults-file=/etc/mysql/debian.cnf "
+        U=$(cat /etc/mysql/debian.cnf | grep ^user | awk '{print $3}' | head -1)
+        P=$(cat /etc/mysql/debian.cnf | grep ^password | awk '{print $3}' | head -1)
+        MYSQL_AUTH=" -u${U} -p${P} "
     fi
 fi
 
