395a8d7232c35cf028a81dadc0fe1ea6b6e3ee89
Juan José Llorens <llorensjj@gmail.com>
2018-07-12 13:01:22 +0200

    Update check_backup.sh

--- mysql/bin/check_backup.sh
+++ mysql/bin/check_backup.sh
@@ -15,20 +15,7 @@ if [ -f "${PATH_BACKUP}" ]; then
     exit 1
 fi
 
-#if [ ! -d "${PATH_BACKUP}" ]; then
-#    mkdir -p "${PATH_BACKUP}"
-#fi
-
-if [ -z ${MYSQL_AUTH} ]; then
-    if [ -f "/etc/psa/.psa.shadow" ];then
-        MYSQL_PASS=`cat /etc/psa/.psa.shadow`
-        MYSQL_AUTH=" -uadmin -p${MYSQL_PASS} "
-    elif [ -f "/etc/mysql/debian.cnf" ];then
-        U=$(cat /etc/mysql/debian.cnf | grep ^user | awk '{print $3}' | head -1)
-        P=$(cat /etc/mysql/debian.cnf | grep ^password | awk '{print $3}' | head -1)
-        MYSQL_AUTH=" -u${U} -p${P} "
-    fi
-fi
+. "${PATH_LIB}/detectauth.sh"
 
 dbs=$(mysql ${MYSQL_AUTH} -BN -e "show databases")
 
@@ -41,7 +28,7 @@ EXIT_CODE=0
 
 JSON_DB_OK=""
 JSON_DB_ERR=""
-PATH_BCK="/opt/descom/mysql_backup/var/"
+
 for db in $dbs;do
     IS_EXCLUDE=0
 
@@ -52,12 +39,12 @@ for db in $dbs;do
     done
 	
     if [ $IS_EXCLUDE == 0 ]; then
-		finded= find ${PATH_BCK}bck_$db.gz -ctime -1 | wc -l > /dev/null
+		finded=$(find ${PATH_BACKUP}bck_$db.gz -ctime -1 | wc -l)
         if [ $? != 0 ];then
             JSON_DB_ERR="${JSON_DB_ERR}${db}; "
             EXIT_CODE=3
         else
-			if [ finded = 1 ];then
+			if [ "$finded" = 1 ];then
 				JSON_DB_OK="${JSON_DB_OK}${db}; "
 			else
 				JSON_DB_ERR="${JSON_DB_ERR}${db}; "
